b1a3eac72eb4b32568a51e0ac1dd65dad9eda553
Nitish Puri <npuri1903@gmail.com>
2018-01-06 23:25:54 +0530

    Adding more examples.

---
+++ html_test/sketch.js
@@ -0,0 +1,77 @@
+var bgColor = 255;
+
+var button;
+var slider;
+
+var angle = 0;
+var angularSpeed = 0.1;
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  background(200);
+  // randomSeed(42)
+
+  button = createButton('Change background');
+  button.position(10, windowHeight - 100);
+  button.mouseClicked(changeBackground);
+
+  slider = createSlider(5, 30, 10, 1);
+  slider.position(10, windowHeight - 50);
+
+  rectMode(CENTER)
+}
+
+function changeBackground() {
+  bgColor = random(100, 200);
+}
+
+function draw() {
+  var r = random(200, 255)
+  var g = random(0, 100)
+  var b = random(100, 200)
+  var a = random(200, 255)
+
+  background(bgColor, bgColor, bgColor, 100);
+  // put drawing code here
+  fill(r, g, b, a);
+  noStroke()
+
+  // circle.X++; 
+  // if (circle.X > width) circle.X = 0;
+  // circle.Y++;
+  // if (circle.Y > height) circle.Y = 0;
+  // circle.sX++;
+  // if (circle.sX > 30) circle.sX = 1;
+  // circle.sY--;
+  // if (circle.sY < 0) circle.sY = 30;
+
+  var s = slider.value() + random(10);
+  ellipse(mouseX, mouseY, s, s);
+  
+  push();
+  translate(100, 100);
+  rotate(angle);
+  fill(g, b, r, a);
+  rect(0, 0, 100, 50);
+  pop();
+
+  push();
+  translate(400, 400);
+  rotate(-3*angle);
+  fill(g, r, b, a);
+  rect(0, 0, 100, 50);
+  pop();
+
+  // stroke(b, g, r, 100)
+  // strokeWeight(3)
+  // line(random(0, width), random(0, height), random(0, width), random(0, height))
+
+  angle += angularSpeed;
+ 
+  // rect(1, 2, 3, h, [tl], [tr], [br], [bl])
+}
+
+function mousePressed() {
+  background(250, 250, 100)
+}
---
+++ prim_mst/sketch.js
@@ -0,0 +1,78 @@
+var vertices = [];
+var edges = []
+
+function setup() {
+  createCanvas(windowWidth - 100, windowHeight - 100)
+
+  for(var i = 0; i < 10; i++){
+    vertices.push(createVector(random(width), random(height)))
+  }
+
+  calcMST();
+}
+
+function mousePressed() {
+  var v = createVector(mouseX, mouseY)
+  vertices.push(v);
+
+  calcMST();
+}
+
+function calcMST() {
+  var reached = [];
+  var unreached = [];
+
+  for (let i = 0; i < vertices.length; i++) {
+    unreached.push(i);
+  }
+
+  reached.push(0);
+  unreached.splice(0, 1);
+
+  // while there are vertices in the unreached array
+  while(unreached.length > 0){
+    var record = 10000;
+    var rIndex;
+    var uIndex;
+
+    // find a closest pair of points connecting the different groups
+    for( var i = 0; i < reached.length; i++){
+      for(var j = 0; j < unreached.length; j++){
+        var v1 = vertices[reached[i]];
+        var v2 = vertices[unreached[j]];
+        var d = dist(v1.x, v1.y, v2.x, v2.y);
+        if(d < record) {
+          record = d;
+          rIndex = i;
+          uIndex = j;
+        }
+      }
+    }
+
+    edges.push(createVector(reached[rIndex], unreached[uIndex]));
+    //line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y)
+
+    reached.push(unreached[uIndex])
+    unreached.splice(uIndex, 1);
+  }
+}
+
+function draw() {
+  background(51)
+
+  
+  // Draw vertices
+  fill(255)
+  stroke(255)
+  for (let i = 0; i < vertices.length; i++) {
+    const v = vertices[i];
+    ellipse(v.x, v.y, 16, 16);
+  }
+
+  // Draw edges
+  stroke(255);
+  strokeWeight(2);
+  for (const edge of edges) {
+    line(vertices[edge.x].x, vertices[edge.x].y, vertices[edge.y].x, vertices[edge.y].y)
+  }
+}
