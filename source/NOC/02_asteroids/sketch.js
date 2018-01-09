ea17679983923fbf1efd78c93a5f8963c846aa25
Nitish Puri <npuri1903@gmail.com>
2018-01-09 18:49:05 +0530

    Adding mouse control to asteroids.

--- source/NOC/02_asteroids/sketch.js
+++ source/NOC/02_asteroids/sketch.js
@@ -6,6 +6,7 @@ var liquid;
 var params = {
   wind: 0,
   moverCount : 5,
+  mouseControl : false,
   reset : function () {
     spaceship = new Spaceship();
     movers = [];
@@ -27,6 +28,7 @@ function setup() {
   var gui = new dat.GUI();
   gui.add(params, 'wind').min(-0.5).max(0.5).step(0.1);
   gui.add(liquid, 'drag').min(0).max(0.1).step(0.01);
+  gui.add(params, 'mouseControl');
   gui.add(params, 'moverCount')
     .min(1).max(10).step(1)
     .onFinishChange(moverCountChanged);
@@ -71,7 +73,7 @@ function draw() {
     }
 
     // 
-    var d = dist(mover.pos.x, mover.pos.y, spaceship.pos.x, spaceship.pos.y);
+    var d = p5.Vector.dist(mover.pos, spaceship.pos);
     if(d < (mover.mass*20 + spaceship.r) ) {
       movers[i] = new Mover(random(width), random(height), random(1, 4));
     }    
@@ -84,19 +86,33 @@ function draw() {
   // Render spaceship after the movers.!!
   spaceship.render();
 
-  // Keyboard controls.
-  if (keyIsPressed) {
-    if ( keyCode === RIGHT_ARROW) {
-      spaceship.turn(0.03);
-    } 
-    else if (keyCode === LEFT_ARROW) {
-      spaceship.turn(-0.03);
-    }
-    else if ( key === ' ') {
+  if(params.mouseControl) {
+    // var h = p5.Vector.sub(createVector(mouseX, mouseY), spaceship.pos).heading();
+    var h = p5.Vector.sub(spaceship.pos, createVector(mouseX, mouseY),).heading();
+
+    h = h - PI/2;
+    constrain(h, -0.05, 0.05);
+
+    spaceship.turn(h - spaceship.heading);
+    if(mouseIsPressed) {
       spaceship.thrust();
     }
-    else if ( key === 'r' || key === 'R') {
-      params.reset();
+  }
+  else{
+    // Keyboard controls.
+    if (keyIsPressed) {
+      if ( keyCode === RIGHT_ARROW) {
+        spaceship.turn(0.03);
+      } 
+      else if (keyCode === LEFT_ARROW) {
+        spaceship.turn(-0.03);
+      }
+      else if ( key === ' ') {
+        spaceship.thrust();
+      }
+      else if ( key === 'r' || key === 'R') {
+        params.reset();
+      }
     }
   }
 }
