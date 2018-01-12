0d1ec4ea0407cc97d34405b67eccfc479c904704
Nitish Puri <npuri1903@gmail.com>
2018-01-12 18:21:44 +0530

    Adding simple smoke demo.

---
+++ source/NOC/04_particles_smoke/particleSystem.js
@@ -0,0 +1,27 @@
+class ParticleSystem {
+    constructor(x, y, img_) {
+        this.particles = [];
+        this.origin = createVector(x, y);
+        this.img = img_;
+    }
+
+    addParticle() {
+        this.particles.push(new Particle(this.origin.x, this.origin.y, this.img));
+    }
+
+    run() {
+        for(let i = this.particles.length - 1; i >= 0; i--) {
+            var p = this.particles[i];
+            p.run();
+            if(p.isDead()) {
+                this.particles.splice(i, 1);
+            }
+        }
+    }
+
+    applyForce(f) {
+        for(var p of this.particles) {
+            p.applyForce(f);
+        }
+    }
+}
