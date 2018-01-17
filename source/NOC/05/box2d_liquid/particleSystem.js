f1e81ac6d73a4b713c755cb770c9e9ed4db63c21
Nitish Puri <npuri1903@gmail.com>
2018-01-17 23:32:32 +0530

    Fix flickering issue due to wrong use of splice.

--- source/NOC/05/box2d_liquid/particleSystem.js
+++ source/NOC/05/box2d_liquid/particleSystem.js
@@ -10,7 +10,7 @@ class ParticleSystem {
     for(let i = this.particles.length - 1; i >= 0; i--) {
       const p = this.particles[i];
       if(p.done()) {
-        this.particles.splice(i);
+        this.particles.splice(i, 1);
       }
       else{
         p.display();
