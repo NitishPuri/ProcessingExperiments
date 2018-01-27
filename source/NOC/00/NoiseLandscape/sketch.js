f48ed6af3684dc03bc5c8aa19d34c34d9282cc23
Nitish Puri <npuri1903@gmail.com>
2018-01-27 22:47:05 +0530

    Visual apheavel to 3d terrain example.

--- source/NOC/00/NoiseLandscape/sketch.js
+++ source/NOC/00/NoiseLandscape/sketch.js
@@ -1,22 +1,35 @@
 let landscape;
 var theta = 0;
 
+var params = {
+  speed : 1
+}
+
 function setup() {
   createCanvasCustom(WEBGL);
 
-  landscape = new Landscape(20, 400, 400);
+  landscape = new Landscape(20, 1000, 1500);
+
+  var gui = new dat.GUI();
+  gui.add(params, 'speed').min(0.1).max(10).step(0.1)
 }
 
 function draw() {
 
-  background(200);
+  const angleX = map(mouseY, 0, height, 0, TWO_PI);
+  const angleZ = map(mouseX, 0, width, 0, TWO_PI);
+  background(255);
 
-  push();
-  translate(100, 100, -160);
-  rotateX(PI/3);
-  rotateZ(theta);
+  // push();
+  // translate(width/2, height/2, 0);
+  // rotateX(angleX);
+  rotateX(PI/2);
+  // rotateZ(theta);  
+  translate(-width/2 - 200, -height -2000, -landscape.w/2);
+  // translate(-landscape.w/2, -landscape.h/2, 0)
+  // rotateZ(angleZ);
   landscape.render();
-  pop();
+  // pop();
 
   landscape.calculate();
 
