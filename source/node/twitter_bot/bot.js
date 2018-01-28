e17fdf2c032db3333270a563e00def2127d0fa87
Nitish Puri <npuri1903@gmail.com>
2018-01-28 18:48:55 +0530

    updating node stuff.

---
+++ source/node/twitter_bot/bot.js
@@ -0,0 +1,12 @@
+console.log("Here we are!")
+
+var Twit = require('twit');
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+// console.log(config)
+
+var T = new Twit(config)
+
