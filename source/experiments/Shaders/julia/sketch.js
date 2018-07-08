3d3fe3db8b56641b6ca331f7870d9b32a1026c80
Nitish Puri <npuri1903@gmail.com>
2018-07-08 14:02:07 +0530

    some colors.

--- source/experiments/Shaders/julia/sketch.js
+++ source/experiments/Shaders/julia/sketch.js
@@ -2,25 +2,27 @@
 
 let params = {
   interactive: true,
+  method: 'getColorGreen',
+  methods: [
+    'getColorGreen',
+    'getColorWheel'
+  ]
 
 }
 
 function setup() {
   console.log("Setting up.");
-  // let gui = new dat.GUI();
+  let gui = new dat.GUI();
   // gui.add(params, 'interactive');
+  gui.add(params, 'method', params.methods).onChange(loadShader)
 
   // setInterval(handleMouse, 100);
 }
 
-// function handleMouse() {
-
-// }
-
-
-
-
 function compile(src) {
+  // return src;
+  console.log(src)
+  src = src.replace(/(#define getColor (.*))/g, "#define getColor " + params.method)
+  console.log(src)
   return src;
-  // return src.replace(/(#define func (.*))/g, "#define func " + params.method)
 }
--- source/experiments/Shaders/julia/untitled.glsl
+++ source/experiments/Shaders/julia/untitled.glsl
@@ -12,17 +12,22 @@ uniform float u_time;
 #define PI 3.141592653589793
 #define TWO_PI 6.283185307179586
 
-vec2 coord(in vec2 p)
-{
+#define getColor getColorGreen
+
+// HSV2RGB
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+vec2 coord(in vec2 p) {
     p = p / u_resolution.xy;
     // correct aspect ratio
-    if (u_resolution.x > u_resolution.y)
-    {
+    if (u_resolution.x > u_resolution.y) {
         p.x *= u_resolution.x / u_resolution.y;
         p.x += (u_resolution.y - u_resolution.x) / u_resolution.y / 2.0;
-    }
-    else
-    {
+    } else {
         p.y *= u_resolution.y / u_resolution.x;
         p.y += (u_resolution.x - u_resolution.y) / u_resolution.x / 2.0;
     }
@@ -31,28 +36,40 @@ vec2 coord(in vec2 p)
     p *= vec2(-1.0, 1.0);
     return p;
 }
+
 #define rx 1.0 / min(u_resolution.x, u_resolution.y)
 #define uv gl_FragCoord.xy / u_resolution.xy
 #define st coord(gl_FragCoord.xy)
 #define mx coord(u_mouse)
 #define aspect (u_resolution.y / u_resolution.x)
 
-#define maxIterations 80
+#define maxIterations 800
+#define colorResolution 256
 
-vec4 getColor(in int iter) {
-    float q = float(iter) / float(maxIterations);
+vec4 getColorGreen(in int iter) {
+    float i = mod(float(iter) + 20.*u_time, float(colorResolution));
+    float q = float(i) / float(colorResolution);
     q = clamp(0., 1., q);
     vec4 c = vec4(0.);
     if(q > 0.5) {
-        c = vec4(q, 1., q, 1.);
+        c = vec4((q-0.5)*2., 1., (q-0.5)*2., 1.);
     } else {
-        c = vec4(0., q, 0., 1.);
+        c = vec4(0., q*2., 0., 1.);
     }
     return c;
 }
-void main()
-{
-    vec2 c = mx;
+
+vec4 getColorWheel(in int iter) {
+    float i = mod(float(iter) + 20.*u_time, float(colorResolution));
+    float q = float(i) / float(colorResolution);
+    q = clamp(0., 1., q);
+    vec4 c = vec4(hsv2rgb(vec3(q, 1.0, 1.0)), 1.0); 
+    return c;
+}
+
+
+void main() {
+    vec2 c = 2.0 * mx;
     // vec2 c = vec2(-0.710,-0.360);
 
     vec2 z;
@@ -62,13 +79,11 @@ void main()
     z.y = 2.0 * pos.y;
 
     int iter = maxIterations;
-    for (int i = 0; i < maxIterations; i++)
-    {
+    for (int i = 0; i < maxIterations; i++) {
         float x = (z.x * z.x - z.y * z.y) + c.x;
         float y = (z.y * z.x + z.x * z.y) + c.y;
 
-        if ((x * x + y * y) > 4.0)
-        {
+        if ((x * x + y * y) > 4.0) {
             iter = i;
             break;
         }
@@ -76,15 +91,6 @@ void main()
         z.y = y;
     }
 
-    // Base the color on the number of iterations
-    // vec4 color;
-    // if (iter == maxIterations)
-    //     color = vec4(0, 0, 0, 1.0); // black
-    // else
-    // {
-    //     float tmpval = float(iter) / 10.0;
-    //     color = vec4(tmpval, tmpval, tmpval, 1.384);
-    // }
-
     gl_FragColor = getColor(iter);
+    // gl_FragColor = getColorWheel(iter);
 }
