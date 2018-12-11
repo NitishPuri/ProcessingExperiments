a18da27b5499a53dbfb21fc0a8e2686474abcada
Nitish Puri <npuri1903@gmail.com>
2018-12-11 15:56:50 +0530

    spice.

--- source/NOC/08/recursion_1/sketch.js
+++ source/NOC/08/recursion_1/sketch.js
@@ -1,14 +1,25 @@
-function setup() {
-  createCanvasCustom();
-  // put setup code here
-}
 
 var t = 0;
+
+let params = {
+  reduction: 0.75
+}
+
+function setup() {
+  createCanvasCustom();
+
+  let gui = new dat.GUI();
+  gui.add(params, 'reduction').min(0.3).max(0.95).step(0.01)
+}
+
+let offset = [15, 4]
 
 function draw() {
   background(51);
   drawCircle(width / 2, height / 2, width);
   t += 0.005;
+  offset[0] += random(-0.1, 0.1)
+  offset[1] += random(-0.1, 0.1)
   // noLoop()
 }
 
@@ -19,6 +30,6 @@ function drawCircle(x, y, r) {
   fill(map(r, width, 0, 100, 255), map(r, width, 0, 255, 150), 0, map(r, width, 0, 100, 255));
   ellipse(x, y, r);
   if (r > 4) {
-    drawCircle(x + 20 * sin(t), y + 5 * cos(t), 0.75 * r);
+    drawCircle(x + offset[0] * sin(t) / params.reduction, y + offset[1] * cos(t) / params.reduction, params.reduction * r);
   }
 }
