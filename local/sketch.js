ee23d6260c025f71a58e7a342f4e9927c44b4880
Nitish Puri <npuri1903@gmail.com>
2018-12-13 01:07:54 +0530

    .

--- local/sketch.js
+++ local/sketch.js
@@ -1,19 +1,57 @@
 
-let synth;
+let synth = undefined;
 
 function setup() {
   // put setup code here
   createCanvasCustom()
   background(255, 0, 200)
-
-  synth = new Tone.Synth().toMaster()
-
+  synth = new Tone.Synth()
+  synth.oscillator.type = 'sine'
+  const gain = new Tone.Gain(0.5);
+  gain.toMaster()
+  synth.connect(gain)
 }
+
+// function getSynth() {
+//   // if (synth == undefined) {
+//   //   console.log("Synth created.")
+//   //   // Tone.context.resume();
+//   //   // StartAudioContext(Tone.context)
+//   // }
+//   // if (Tone.context.state !== "running") {
+//   //   console.log("Audio resumed.");
+//   // }
+//   return synth;
+// }
 
 function keyPressed(key) {
   // console.log(key.key)
   if (key.key == 'p') {
-    synth.triggerAttackRelease("C4", "8n")
+    // console.log("Playing.")
+    // let synth = getSynth();
+
+    const notes = [
+      'C4', 'E4', 'G4',
+      'C5', 'E5', 'G5'
+    ]
+
+    let index = 0;
+
+    Tone.Transport.scheduleRepeat(repeat, '8n');
+
+    function repeat(time) {
+      // let note = notes[int(random(10)) % notes.length];
+      let note = notes[index % notes.length];
+      synth.triggerAttackRelease(note, '8n', time);
+      index++;
+    }
+
+    Tone.Transport.start();
+
+    setTimeout(() => {
+      Tone.Transport.stop();
+    }, 2000);
+
   }
 }
 
